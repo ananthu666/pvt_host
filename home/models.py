1d151c16b11f622f2e67c45d5eb0611fe8cace2a
Ananthu Jayakumar <115489088+ananthu666@users.noreply.github.com>
2023-11-26 17:13:47 +0530

    new updations

--- home/models.py
+++ home/models.py
@@ -5,7 +5,9 @@ class Hostel(models.Model):
     vacancy = models.IntegerField()
     lat = models.FloatField()
     lng = models.FloatField()
+    phone = models.CharField(max_length=15,default="nil")  # Adjust the max_length as needed
+    mess_availability = models.CharField(max_length=15, default="NO")
+    distance_from_college = models.FloatField(max_length=15,default=0.0)
 
     def __str__(self):
         return self.name
-
